644eb8b26a78cbb8bd0f88716d37964b0ef4a8e3
Tiago Vinícius <tiagovinicyus1996@gmail.com>
2023-03-03 00:43:12 +0000

    new content
    new content and basement added

---
+++ script/code.js
@@ -0,0 +1,44 @@
+function openMenu() {
+    let menu = document.getElementById('menu')
+    menu.classList.toggle('active')
+}
+
+function openSite1() {
+    let link = document.getElementById('backgroundPortfolio1')
+    link.style.display = 'flex'
+}
+
+function closeSite1() {
+    let link = document.getElementById('backgroundPortfolio1')
+    link.style.display = 'none'
+}
+
+function openSite2() {
+    let link = document.getElementById('backgroundPortfolio2')
+    link.style.display = 'flex'
+}
+
+function closeSite2() {
+    let link = document.getElementById('backgroundPortfolio2')
+    link.style.display = 'none'
+}
+
+function openSite3() {
+    let link = document.getElementById('backgroundPortfolio3')
+    link.style.display = 'flex'
+}
+
+function closeSite3() {
+    let link = document.getElementById('backgroundPortfolio3')
+    link.style.display = 'none'
+}
+
+function openSite4() {
+    let link = document.getElementById('backgroundPortfolio4')
+    link.style.display = 'flex'
+}
+
+function closeSite4() {
+    let link = document.getElementById('backgroundPortfolio4')
+    link.style.display = 'none'
+}
